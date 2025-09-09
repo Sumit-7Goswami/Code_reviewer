54c9f48bfd042b68ec4140edc51f0485b8c7c07a
Sumit-7Goswami <sumitgoswami9916@gmail.com>
2025-09-09 21:31:29 +0530

    done for render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,8 +21,9 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
-    setReview(response.data)
+    const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+    const response = await axios.post(`${API_URL}/ai/get-review`, { code })
+    setReview(response.data) ;
   }
 
   return (
